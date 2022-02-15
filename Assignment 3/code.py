eb3f79fa8b350207f48532dd072d8ba2c40bc879
Yash Gupta <yashbg7@gmail.com>
2022-02-16 05:19:27 +0530

    Simplify functions

--- Assignment 3/code.py
+++ Assignment 3/code.py
@@ -1,16 +1,10 @@
 def modInverse(a, m):
-    # If a and m are relatively prime,
-    # then modulo inverse is a^(m-2) mode m
     return power(a, m - 2, m)
- 
-# To compute x^y under modulo m
- 
- 
+
 def power(x, y, m):
- 
     if (y == 0):
         return 1
- 
+
     p = power(x, y // 2, m) % m
     p = (p * p) % m
  
@@ -19,20 +13,13 @@ def power(x, y, m):
     else:
         return ((x * p) % m)
 
-def solve(b, i, j, k):
+def solve(b, c):
     ans = 1
-    if i < 0:
-        ans *= power(modInverse(b[0], p), i, p)
-    else:
-        ans *= power(b[0], i, p)
-    if j < 0:
-        ans *= power(modInverse(b[1], p), j, p)
-    else:
-        ans *= power(b[1], j, p)
-    if k < 0:
-        ans *= power(modInverse(b[2], p), k, p)
-    else:
-        ans *= power(b[2], k, p)
+    for i in range(len(c)):
+        if c[i] < 0:
+            ans = (ans * power(modInverse(b[i], p), -c[i], p)) % p
+        else:
+            ans = (ans * power(b[i], c[i], p)) % p
     return ans
 
 
@@ -52,10 +39,9 @@ for i in range(-100, 101):
         for k in range(-100, 101):
             if i * a[0] + j * a[1] + k * a[2] == 1:
                 c.append([i, j, k])
-                # print(i, j, k)
 
 print(c)
 
 for i in range(len(c)):
-    g = solve(b, c[i][0], c[i][1], c[i][2])
+    g = solve(b, c[i])
     print(g)
