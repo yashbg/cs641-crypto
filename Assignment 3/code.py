84a859fc8a2f32a4ab0cea568fb0c50d3d312257
Yash Gupta <yashbg7@gmail.com>
2022-02-16 17:02:29 +0530

    Use pow()

--- Assignment 3/code.py
+++ Assignment 3/code.py
@@ -1,28 +1,13 @@
-from operator import mod
-
-
-def power(x, y, m):
-    if (y == 0):
-        return 1
-
-    p = power(x, y // 2, m) % m
-    p = (p * p) % m
- 
-    if(y % 2 == 0):
-        return p
-    else:
-        return ((x * p) % m)
-
-def modInverse(a):
-    return power(a, p - 2, p)
+def inverse(a):
+    return pow(a, p - 2, p)
 
 def solve(b, c):
     ans = 1
     for i in range(len(c)):
         if c[i] < 0:
-            ans = (ans * power(modInverse(b[i]), -c[i], p)) % p
+            ans = (ans * pow(inverse(b[i]), -c[i], p)) % p
         else:
-            ans = (ans * power(b[i], c[i], p)) % p
+            ans = (ans * pow(b[i], c[i], p)) % p
     return ans
 
 p = 455470209427676832372575348833
@@ -31,13 +16,17 @@ a = [429, 1973, 7596]
 b = [
     431955503618234519808008749742,
     176325509039323911968355873643,
-    98486971404861992487294722613
+    98486971404861992487294722613,
 ]
 
-a_dif = [a[1] - a[0], a[2] - a[1]]
+a_dif = [
+    a[1] - a[0],
+    a[2] - a[1],
+]
+
 b_dif = [
-    (b[1] * modInverse(b[0])) % p,
-    (b[2] * modInverse(b[1])) % p
+    (b[1] * inverse(b[0])) % p,
+    (b[2] * inverse(b[1])) % p,
 ]
 
 print(a_dif)
@@ -50,17 +39,17 @@ for i in range(-10000, 10000):
         c.append([i, j])
 print(c)
 
-gs = []
+g_list = []
 for i in range(len(c)):
-    gs.append(solve(b_dif, c[i]))
+    g_list.append(solve(b_dif, c[i]))
 
-for g in sorted(gs):
+for g in sorted(g_list):
     print(g)
 print()
 
 g = 52565085417963311027694339
 coef = [-2298, 631]
 
-g_inv = modInverse(g)
-pwd = (b[0] * power(g_inv, a[0], p)) % p
+g_inv = inverse(g)
+pwd = (b[0] * pow(g_inv, a[0], p)) % p
 print(pwd)
